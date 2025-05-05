ce0a013b2577b8274622d198418dd7718756a4a1
yajinx23 <papayat49@gmail.com>
2025-05-05 22:22:00 +0000

    page connexion et dinscription relie avec le back

--- gestion_donation/routes/web.php
+++ gestion_donation/routes/web.php
@@ -1,20 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 
 // Routes pour les pages principales
 Route::get('/', function () {
     return view('welcome');
 })->name('accueil');
 
-// Routes pour les pages d'authentification
-Route::get('/connexion-donateur', function() {
-    return view('donateur.connexiondonateur');
-})->name('donateur.connexion');
+// Routes pour l'authentification donateur
+Route::get('/connexion-donateur', [LoginController::class, 'showLoginForm'])
+    ->name('donateur.connexion');
 
+Route::post('/connexion-donateur', [LoginController::class, 'login'])
+    ->name('donateur.login');
+
+Route::post('/deconnexion-donateur', [LoginController::class, 'logout'])
+    ->name('donateur.logout');
+
+// Routes pour l'inscription donateur
 Route::get('/inscription-donateur', function() {
     return view('donateur.incription');
 })->name('donateur.inscription');
+
+Route::post('/inscription-donateur', [RegisterController::class, 'register'])
+    ->name('donateur.register');
 
 Route::get('/connexion-admin', function() {
     return view('admin.connexionadmin');
